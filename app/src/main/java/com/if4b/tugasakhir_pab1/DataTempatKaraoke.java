06f1fe847f16acebb5bf9662c86ae83a59f68efe
Rgafril90 <93838735+Rgafril90@users.noreply.github.com>
2022-05-25 10:47:59 +0700

    tampilan xml

---
+++ app/src/main/java/com/if4b/tugasakhir_pab1/DataTempatKaraoke.java
@@ -0,0 +1,8 @@
+package com.if4b.tugasakhir_pab1;
+
+public class DataTempatKaraoke {
+
+    public static String[][] data = new String[][] {
+            {"Diva Family Karaoke", "Bar Karaoke"}
+    };
+}
